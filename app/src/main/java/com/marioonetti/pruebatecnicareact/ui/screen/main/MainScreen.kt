5f2b6979277659bc6dc2fb2d2a4ed0e1f397b62f
Mario Onetti <maarioonettiolmos@gmail.com>
2025-09-20 15:44:34 +0200

    Images comparision completed

--- app/src/main/java/com/marioonetti/pruebatecnicareact/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/marioonetti/pruebatecnicareact/ui/screen/main/MainScreen.kt
@@ -6,13 +6,15 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,8 +23,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
 import com.marioonetti.pruebatecnicareact.ui.shared.LoadingComposable
 
 @Composable
@@ -35,9 +35,6 @@ fun MainScreen(
         is MainState.Idle -> { MainBodyComposable(state, onEvent) }
         is MainState.Loading -> {
             LoadingComposable()
-        }
-        is MainState.Error -> {
-            Column(modifier = modifier) {}
         }
     }
 }
@@ -61,37 +58,72 @@ fun MainBodyComposable(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        state.image?.let {
-            Image(
-                bitmap = it.asImageBitmap(),
-                contentDescription = "Selfie",
-                modifier = Modifier.size(150.dp),
-                contentScale = ContentScale.Crop
-            )
-        }
-
-        Row {
-            Button(
-                onClick = { onEvent(MainEvent.OnFaceCaptureClick) }
-            ) {
-                Text("Capturar rostro")
-            }
-
-            Button(
-                onClick = {
-                    val isAvailable =
-                        ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(context)
-                    if (isAvailable) {
-                        pickSingleMedia.launch(
-                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                        )
+        if (state.showErrorPopUp) {
+            ElevatedCard {
+                Column {
+                    Text(state.errorMessage ?: "Error")
+                    Button(
+                        onClick = {
+                            onEvent(MainEvent.OnCloseError)
+                        }
+                    ) {
+                        Text("Aceptar")
                     }
                 }
+            }
+        } else {
+            Row {
+                state.capturedImage?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = "captured Image",
+                        modifier = Modifier.size(150.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                state.galleryImage?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = "Gallery image",
+                        modifier = Modifier.size(150.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+
+            state.percentage?.let {
+                Text("El porcentaje de similitud es: $it")
+            }
+
+            Row {
+                Button(
+                    onClick = { onEvent(MainEvent.OnFaceCaptureClick) }
+                ) {
+                    Text("Capturar rostro")
+                }
+
+                Button(
+                    onClick = {
+                        val isAvailable =
+                            ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable(context)
+                        if (isAvailable) {
+                            pickSingleMedia.launch(
+                                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                            )
+                        }
+                    }
+                ) {
+                    Text("Seleccionar imagen")
+                }
+            }
+
+            Button(
+                onClick = { onEvent(MainEvent.OnCompareImagesClicked) }
             ) {
-                Text("Seleccionar imagen")
+                Text("Comparar imagenes")
             }
         }
-
     }
 }
 
--- app/src/main/java/com/marioonetti/pruebatecnicareact/ui/screen/main/MainViewModel.kt
+++ app/src/main/java/com/marioonetti/pruebatecnicareact/ui/screen/main/MainViewModel.kt
@@ -14,6 +14,12 @@ import com.marioonetti.pruebatecnicareact.ui.screen.ViewEvent
 import com.marioonetti.pruebatecnicareact.ui.screen.ViewState
 import com.regula.facesdk.FaceSDK
 import com.regula.facesdk.configuration.FaceCaptureConfiguration
+import com.regula.facesdk.configuration.MatchFacesConfiguration
+import com.regula.facesdk.enums.ImageType
+import com.regula.facesdk.enums.ProcessingMode
+import com.regula.facesdk.model.MatchFacesImage
+import com.regula.facesdk.model.results.matchfaces.MatchFacesResponse
+import com.regula.facesdk.request.MatchFacesRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -34,18 +40,21 @@ class MainViewModel(
             }
 
             is MainEvent.OnGalleryImageSelected -> {
-                event.uri?.let {
-                    viewModelScope.launch {
-                        val bitmap = uriToBitmap(appContext, event.uri)
-                        updateState { MainState.Idle(image = bitmap) }
-                    }
-                }
+                handleUri(event.uri)
+            }
+
+            is MainEvent.OnCompareImagesClicked -> {
+                compareImages()
+            }
+
+            is MainEvent.OnCloseError -> {
+                closeError()
             }
         }
     }
 
 
-    fun presentFaceCapture() {
+    private fun presentFaceCapture() {
 
         val configuration = FaceCaptureConfiguration.Builder()
             .setCameraId(0)
@@ -54,16 +63,61 @@ class MainViewModel(
 
         FaceSDK.Instance().presentFaceCaptureActivity(appContext, configuration) { response ->
             response.exception?.let {
-                updateState { MainState.Error(it.message ?: "") }
+                val currentState = uiState.value as MainState.Idle
+                updateState { currentState.copy(errorMessage = it.message, showErrorPopUp = true) }
             }
 
             val image = response.image
             val bitmap = image?.bitmap
-
+            val currentState = uiState.value as MainState.Idle
             updateState {
-                MainState.Idle(image = bitmap)
+                currentState.copy(capturedImage = bitmap)
             }
         }
+    }
+
+    private fun compareImages() {
+        val currentState = uiState.value as MainState.Idle
+        val request = MatchFacesRequest(
+            listOf(
+                MatchFacesImage(currentState.capturedImage, ImageType.PRINTED),
+                MatchFacesImage(currentState.galleryImage, ImageType.PRINTED),
+            )
+        )
+
+        val configuration =
+            MatchFacesConfiguration.Builder().setProcessingMode(ProcessingMode. OFFLINE).build()
+        FaceSDK.Instance().matchFaces(
+            appContext, request, configuration
+        ) { response: MatchFacesResponse? ->
+            if (response != null) {
+                if (response.exception != null) {
+                    updateState { currentState.copy(errorMessage = response.exception.message ?: "", showErrorPopUp = true) }
+                } else {
+                    val percentage = response.results?.first()?.similarity
+                    updateState {
+                        currentState.copy(percentage = percentage)
+                    }
+                }
+            } else {
+                updateState { currentState.copy(errorMessage = "Empty response, try it later...", showErrorPopUp = true) }
+            }
+        }
+    }
+
+    private fun handleUri(uri: Uri?) {
+        uri?.let {
+            viewModelScope.launch {
+                val bitmap = uriToBitmap(appContext, uri)
+                val currentState = uiState.value as MainState.Idle
+                updateState { currentState.copy(galleryImage = bitmap) }
+            }
+        }
+    }
+
+    private fun closeError() {
+        val currentState = uiState.value as MainState.Idle
+        updateState { currentState.copy(showErrorPopUp = false) }
     }
 
     private suspend fun uriToBitmap(context: Context, uri: Uri): Bitmap? = withContext(Dispatchers.IO) {
@@ -79,16 +133,19 @@ class MainViewModel(
 sealed class MainState: ViewState() {
     data object Loading: MainState()
     data class Idle(
-        val image: Bitmap? = null,
-        val galleryUri: Uri? = null
+        val capturedImage: Bitmap? = null,
+        val galleryImage: Bitmap? = null,
+        val percentage: Double? = null,
+        val errorMessage: String? = null,
+        val showErrorPopUp: Boolean = false,
     ): MainState()
-    data class Error(val message: String): MainState()
 }
 
 sealed class MainEvent: ViewEvent() {
     data object OnFaceCaptureClick: MainEvent()
     data object OnSelectGalleryClick: MainEvent()
-
+    data object OnCompareImagesClicked: MainEvent()
+    data object OnCloseError: MainEvent()
     data class OnGalleryImageSelected(
         val uri: Uri?
     ): MainEvent()
